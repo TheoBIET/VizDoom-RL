cee778ec012efd2594de1c523cbea28b8abdfd5f
Théo Biet <dev.theobiet@gmail.com>
2022-09-05 17:23:59 +0200

    🧠 | Add 40k trained defend_center model

--- play.py
+++ play.py
@@ -3,14 +3,17 @@ import time
 # GymEnv Class
 from classes.GymEnv import GymEnv
 # Usefull constants
-from utils.constants import MODEL_PATH, N_EVAL_EPISODES
+from utils.constants import N_EVAL_EPISODES, DEFEND_CENTER_SCENARIO
 # Stable Baselines
 from stable_baselines3 import PPO
 # Import Evaluation Policy to test our agent
 from stable_baselines3.common.evaluation import evaluate_policy
 
+MODEL_PATH='./models/defend_center_0509'
+SCENARIO=DEFEND_CENTER_SCENARIO
+
 model = PPO.load(MODEL_PATH)
-env = GymEnv(render=True, hd=True)
+env = GymEnv(scenario_path=SCENARIO, render=True, hd=True)
 
 mean_reward, _ = evaluate_policy(model, 
                                  env, 
